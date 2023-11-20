544e35defc78250f397eca73bbb9525879f0556e
necro <usedforstupidgamesandshit@gmail.com>
2023-11-20 17:32:08 -0600

    huge change

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({
-	site: 'https://necrothon.com',
-	integrations: [mdx(), sitemap()],
-});
+export default defineConfig({});
